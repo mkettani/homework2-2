c59a40f69c6d95cbba99443d7569a5170dad1c90
mkettani <mehdikettani89@gmail.com>
2017-02-15 15:04:31 -0500

    Update display_results.php

--- display_results.php
+++ display_results.php
@@ -6,7 +6,6 @@
         FILTER_VALIDATE_FLOAT);
     $years = filter_input(INPUT_POST, 'years',
         FILTER_VALIDATE_INT);
-
     // validate investment
     if ($investment === FALSE ) {
         $error_message = 'Investment must be a valid number.'; 
@@ -15,8 +14,10 @@
     // validate interest rate
     } else if ( $interest_rate === FALSE )  {
         $error_message = 'Interest rate must be a valid number.'; 
-    } else if ( $interest_rate <= 0 ) {
-        $error_message = 'Interest rate must be greater than zero.'; 
+    } else if ( $interest_rate <= 0) {
+        $error_message = 'Interest rate must be greater than zero .';
+    } else if ( $interest_rate > 15 ) {
+        $error_message = 'Interest rate must be less than or equal to 15.';
     // validate years
     } else if ( $years === FALSE ) {
         $error_message = 'Years must be a valid whole number.';
@@ -26,21 +27,17 @@
         $error_message = 'Years must be less than 31.';
     // set error message to empty string if no invalid entries
     } else {
-        $error_message = ''; 
-    }
-
+        $error_message = ''; }
     // if an error message exists, go to the index page
     if ($error_message != '') {
         include('index.php');
-        exit(); }
-
+        exit();
+    }
     // calculate the future value
     $future_value = $investment;
     for ($i = 1; $i <= $years; $i++) {
-        $future_value = 
-            $future_value + ($future_value * $interest_rate * .01); 
+        $future_value = $future_value + ($future_value * $interest_rate *.01);
     }
-
     // apply currency and percent formatting
     $investment_f = '$'.number_format($investment, 2);
     $yearly_rate_f = $interest_rate.'%';
@@ -50,23 +47,25 @@
 <html>
 <head>
     <title>Future Value Calculator</title>
-    <link rel="stylesheet" type="text/css" href="main.css">
+    <link rel="stylesheet" type="text/css" href="main.css"/>
 </head>
 <body>
     <main>
         <h1>Future Value Calculator</h1>
 
         <label>Investment Amount:</label>
-        <span><?php echo $investment_f; ?></span><br>
+        <span><?php echo htmlspecialchars($investment_f); ?></span><br />
 
         <label>Yearly Interest Rate:</label>
-        <span><?php echo $yearly_rate_f; ?></span><br>
+        <span><?php echo htmlspecialchars($yearly_rate_f); ?></span><br />
 
         <label>Number of Years:</label>
-        <span><?php echo $years; ?></span><br>
+        <span><?php echo htmlspecialchars($years); ?></span><br />
 
         <label>Future Value:</label>
-        <span><?php echo $future_value_f; ?></span><br>
+        <span><?php echo htmlspecialchars($future_value_f); ?></span><br />
+        
+        <p>This calculation was done on <?php echo date('m/d/Y'); ?>.</p>
     </main>
 </body>
 </html>
